05485a503d36af880dafbeaee5928d8a5a74067b
yuna950 <dbsdk4103@naver.com>
2026-07-31 11:10:35 +0900

    fix:base

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/yuna950-movie_review_app-26_07_22.git/",
 });
